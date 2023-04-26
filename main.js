f943f4b01c4c2f90fe63496e86b25dd96384178d
Marisa Wyatt <risawyatt22@gmail.com>
2023-04-25 21:30:33 -0600

    Add query selectors

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
 // query Selectors
-// var gameSection = document.querySelectorAll(".game-buttons")
+var gameSection = document.querySelectorAll(".game-buttons")
+var changeGameButton = document.querySelector(".game-choice-button")
+var easyGameRules = document.querySelector("#easy-rules")
+var hardGameRules = document.querySelector()
+var rock = document.querySelector("#rock")
+var paper = document.querySelector("#paper")
+var scissors = document.querySelector("#scissors")
+var lizard = document.querySelector("#lizard")
+var alien = document.querySelector("#alien")
 
 // Event listeners
 
@@ -39,4 +47,53 @@ function takeTurn(game) {
         var random = getRandomIndex(easyOptions)
         game.computer.selection = easyOptions[random]
     }
+    game.human.selection = "paper"
+    return game
+}
+
+function determineEasyWinner(game) {
+    var easyWinner = [
+        'rock > scissors',
+        'paper > rock',
+        'scissors > paper'
+    ]
+        if (easyWinner.includes(`${game.human.selection} > ${game.computer.selection}`) ){
+            game.human.wins += 1
+            return 'human wins!'
+        } else if (game.human.selection === game.computer.selection){
+            return 'DRAW!'
+        } else {
+            game.computer.wins += 1
+            return 'computer wins!'
+        }
+    }
+    
+function determineHardWinner(game) {   
+    var hardWinner = [
+        'rock > scissors',
+        'rock > lizard',
+        'paper > rock',
+        'paper > alien',
+        'scissors > paper',
+        'scissors > lizard',
+        'lizard > paper',
+        'lizard > alien',
+        'alien > scissors', 
+        'alien > rock'
+    ]
+    if (hardWinner.includes(`${game.human.selection} > ${game.computer.selection}`) ){
+        game.human.wins += 1
+        return 'human wins!'
+    } else if (game.human.selection === game.computer.selection){
+        return 'DRAW!'
+    } else {
+        game.computer.wins += 1
+        return 'computer wins!'
+    }
+}
+
+function resetBoard(game) {
+    game.computer.wins = 0;
+    game.human.wins = 0;
+    return game
 }
