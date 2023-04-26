4b5defa079a716a459fa3cfdfd02f9142f442926
Marisa Wyatt <risawyatt22@gmail.com>
2023-04-26 10:06:36 -0600

    Add the option of game choice

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 // query Selectors
 var gameSection = document.querySelectorAll(".game-buttons")
 var changeGameButton = document.querySelector(".game-choice-button")
-var easyGameRules = document.querySelector("#easy-rules")
-var hardGameRules = document.querySelector()
+var easyGameRules = document.querySelector(".easy-rules")
+var hardGameRules = document.querySelector(".hard-rules")
+var rulesWrapper = document.querySelector(".rules-wrapper")
+var textbox = document.querySelector(".text-box")
 var rock = document.querySelector("#rock")
 var paper = document.querySelector("#paper")
 var scissors = document.querySelector("#scissors")
@@ -10,12 +12,42 @@ var lizard = document.querySelector("#lizard")
 var alien = document.querySelector("#alien")
 
 // Event listeners
+window.addEventListener("load", function(event){
+    event.preventDefault();
+    // chooseGame()
+})
 
+easyGameRules.addEventListener("click", chooseEasyGame)
+hardGameRules.addEventListener("click", chooseHardGame)
 // global variables
 
 var easyOptions = ["rock", "paper", "scissors"]
-var hardOptions = ["rock", "paper", "scissors", "lizars", "alien"]
+var hardOptions = ["rock", "paper", "scissors", "lizard", "alien"]
 var game = createGame(); 
+
+function chooseEasyGame() {
+        show(rock);
+        show(paper);
+        show(scissors);
+        hide(rulesWrapper)
+        textbox.innerText = `choose your fighter`
+        // playEasyGame function
+    }
+
+function chooseHardGame() {
+        show(rock);
+        show(paper);
+        show(scissors);
+        show(lizard);
+        show(alien);
+        hide(rulesWrapper)
+        textbox.innerText = "choose your fighter"
+        // playHardGame function
+}
+// function playEasyGame() {
+    // creategame()
+    // on user click have computer take turn
+// }
 
 function createPlayer(name, token) {
     var player = {
@@ -96,4 +128,12 @@ function resetBoard(game) {
     game.computer.wins = 0;
     game.human.wins = 0;
     return game
+}
+
+function hide(element) {
+    element.setAttribute("hidden", "")
+}
+
+function show(element) {
+    element.removeAttribute("hidden")
 }
